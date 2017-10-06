e2dd7abc7ff047b16914fbd9348cb8868984642f
Faltynkova <Anezka.Faltynkova@gorkana.com>
2017-10-06 14:54:09 +0200

    Spring boot initialization

---
+++ src/main/java/com/faltynka/faltynkaapi/FaltynkaApiApplication.java
@@ -0,0 +1,12 @@
+package com.faltynka.faltynkaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaltynkaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaltynkaApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/faltynka/faltynkaapi/FaltynkaApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.faltynka.faltynkaapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FaltynkaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
